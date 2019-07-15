8a2c4a1880fcae89040e2fe1a903bac8b8b873cf
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-15 17:17:37 +0900

    msm-poweroff: do warm resets only on panic and disable download mode
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Sun Jul 22 22:05:13 2018 +0300
    
        power: reset: Move in_panic handling out of dload mode
    
        Change-Id: Idd4a2cc8a47041740f8d4e9f43bffd84fae5830d
    
    Change-Id: I7e31f24f0db937a9304334b7897e5f1e0ac83643

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -88,7 +88,6 @@ static struct notifier_block panic_blk = {
 #define KASLR_OFFSET_PROP "qcom,msm-imem-kaslr_offset"
 #endif
 
-static struct kobject dload_kobj;
 static int dload_type = SCM_DLOAD_FULLDUMP;
 static void *dload_mode_addr;
 static void *dload_type_addr;
@@ -158,10 +157,12 @@ static void set_dload_mode(int on)
 	dload_mode_enabled = on;
 }
 
+#ifdef CONFIG_QCOM_DLOAD_MODE
 static bool get_dload_mode(void)
 {
 	return dload_mode_enabled;
 }
+#endif
 
 static void enable_emergency_dload_mode(void)
 {
@@ -230,10 +231,12 @@ static void enable_emergency_dload_mode(void)
 	pr_err("dload mode is not enabled on target\n");
 }
 
+#ifdef CONFIG_QCOM_DLOAD_MODE
 static bool get_dload_mode(void)
 {
 	return false;
 }
+#endif
 #endif
 
 static void scm_disable_sdi(void)
@@ -288,7 +291,6 @@ static void halt_spmi_pmic_arbiter(void)
 
 static void msm_restart_prepare(const char *cmd)
 {
-	bool need_warm_reset = false;
 #ifdef CONFIG_QCOM_DLOAD_MODE
 	/* Write download mode flags if we're panic'ing
 	 * Write download mode flags if restart_mode says so
@@ -299,22 +301,8 @@ static void msm_restart_prepare(const char *cmd)
 			(in_panic || restart_mode == RESTART_DLOAD));
 #endif
 
-	if (qpnp_pon_check_hard_reset_stored()) {
-		/* Set warm reset as true when device is in dload mode */
-		if (get_dload_mode() ||
-			((cmd != NULL && cmd[0] != '\0') &&
-			!strcmp(cmd, "edl")))
-			need_warm_reset = true;
-	} else {
-		need_warm_reset = (get_dload_mode() ||
-				(cmd != NULL && cmd[0] != '\0'));
-	}
-
-	if (force_warm_reboot)
-		pr_info("Forcing a warm reset of the system\n");
-
 	/* Hard reset the PMIC unless memory contents must be maintained. */
-	if (force_warm_reboot || need_warm_reset)
+	if (in_panic)
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
 	else
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_HARD_RESET);

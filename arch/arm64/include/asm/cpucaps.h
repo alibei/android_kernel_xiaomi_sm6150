493a727499be1a3626de9ea8e401891edf05050b
bsp-open <bsp-open@xiaomi.com>
2022-06-30 00:08:29 +0200

    Kernel: Xiaomi kernel changes for Redmi K20, Redmi K30, MI Note 10 Lite and MI CC9 Pro Android R
    [alibei]
    * Remove binder changes
    * Remove bootinfo driver
    * Remove bpf changes
    * Remove brightness clone
    * Remove camera disable dump state
    * Remove cpufreq changes
    * Remove debug policy status
    * Remove defconfigs
    * Remove dump all stacks
    * Remove empty changes
    * Remove factory build
    * Remove fastboot uart
    * Remove FIQ debugger
    * Remove fire watchdog
    * Remove fpc_tee driver
    * Remove fs changes
    * Remove fs_tee fingerprint driver
    * Remove gpio test mode
    * Remove gpu/drm changes
    * Remove gpu/msm changes
    * Remove gt9897t touchscreen driver
    * Remove hardware info manager
    * Remove IRQ boost
    * Remove kernel/trace changes
    * Remove low memory killer changes
    * Remove lpm level changes
    * Remove md changes
    * Remove microdump changes
    * Remove mm changes
    * Remove msm/camera_v3
    * Remove net changes
    * Remove perf helper
    * Remove poweroff changes
    * Remove qseecom changes
    * Remove sched changes
    * Remove sde/rotator changes
    * Remove skip debug fs changes
    * Remove spi rt prio
    * Remove watchdog v2 changes
    * Remove xlogchar driver
    
    The Patch based on QualComm release TAG:LA.UM.9.1.r1-06700-SMxxx0.0-1
    The kernel config file used is davinci_user_defconfig, phoenix_user_defconfig, toco_user_defconfig and tucana_user_defconfig
    
    Change-Id: Iab1174fe1304d2a110074e1b0084558ebd621620
    Signed-off-by: bsp-open <bsp-open@xiaomi.com>

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -2,6 +2,7 @@
  * arch/arm64/include/asm/cpucaps.h
  *
  * Copyright (C) 2016 ARM Ltd.
+ * Copyright (C) 2021 XiaoMi, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
